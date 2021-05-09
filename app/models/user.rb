9db3f297e0fb95131eab155c6dc871300b315b83
Fares <ashraffares090@gmail.com>
2021-05-09 22:27:05 +0200

    Configure devise to add name field

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true
   has_many :posts
 end
