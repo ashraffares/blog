8a204119d51073a5121dcfde566e28d8dfb035d4
Fares <ashraffares090@gmail.com>
2021-05-09 21:20:43 +0200

    Add delete method and link to in show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,6 +34,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
 
   def post_params
