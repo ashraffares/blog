a5e7b2478a7dcacf80a27355f9dce0f1df53df8a
Fares <ashraffares090@gmail.com>
2021-05-09 21:39:54 +0200

    Add Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_094605) do
+ActiveRecord::Schema.define(version: 2021_05_09_193822) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -31,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_05_09_094605) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
