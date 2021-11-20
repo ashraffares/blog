f6c753a56ff9cb7cccbc38af2c8b3812355116e5
ashraffares <ashraffares090@gmail.com>
2021-11-20 07:09:43 +0200

    remove json format

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,29 +39,11 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /posts/1 or /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    @post = current_user.posts.new(user_id: params[:user_id], title: params[:title], text: params[:text])
+    if @post.save
+      redirect_to posts_path, notice: 'Create post successfully'
+    else
+      redirect_to posts_path, notice: 'Something went wrong Please try again!'
     end
   end
 
@@ -79,10 +61,5 @@ class PostsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def post_params
-    params.require(:post).permit(:title, :text, :commentsCounter, :likesCounter, :user_id)
   end
 end
