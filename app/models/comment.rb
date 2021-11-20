a877fbfa3d58967fd107bc5ace4e897c84c5fad7
ashraffares <ashraffares090@gmail.com>
2021-11-20 06:08:37 +0200

    Add association and validation for (post-comment-like) model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  validates :text, presence: true
+
+  def update_comments_counter(id)
+    post = Post.find_by(id: id)
+    post.commentsCounter += 1
+    post.update
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  validates :title, :text, presence: true
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, length: { maximum: 250 }
+  after_save :update_post_counter
+
+  def update_post_counter
+    user.update(postsCounter: user.posts.size)
+  end
+
+  def last_comments(id)
+    Post.find_by(id: id).comments.last(5)
+  end
 end
