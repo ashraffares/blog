1a209f91ce112ae6d2a328891711dd697705fd46
ashraffares <ashraffares090@gmail.com>
2021-11-20 06:45:34 +0200

    Add seed and edit routes folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :posts
   root to: 'users#index'
   devise_for :users
+  get '/users/:user_id/posts/:id/destroy', to: 'posts#destroy', as: 'posts/destroy'
+  get '/comments/:id', to: 'comments#destroy', as: 'posts/destroy/comment'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:user_id/posts', to: 'posts#index', as: "posts"
+  post '/users/:user_id/posts', to: 'posts#create'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'posts/show'
+  post '/users/:user_id/posts/:id/like', to: 'posts#like', as: 'posts/like'
+
+  post '/users/:user_id/posts/comment', to: 'comments#create', as: "comments"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([{name: 'carlo', email: 'dfffff@gmail.com', password: 'fwafafawf', photo: 'https://images-eu.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX198_BO1,204,203,200_QL40_ML2_.jpg', postsCounter:1, bio: 'Bio of the user'},
+    {name: 'marco', email: 'dfjiafwfwaffawhf@gmail.com', password: 'fwafafawf', photo: 'https://images-eu.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX198_BO1,204,203,200_QL40_ML2_.jpg', postsCounter:1, bio: 'Bio of the user'},
+    {name: 'giulio', email: 'dffffff@gmail.com', password: 'fwafafawf', photo: 'https://images-eu.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX198_BO1,204,203,200_QL40_ML2_.jpg', postsCounter:1, bio: 'Bio of the user'}])
+
+Post.create!([{ user_id: 1, title: 'post1', text: 'beautiful', commentsCounter: 3, likesCounter: 3 },
+     { user_id: 1, title: 'post2', text: 'beautiful1', commentsCounter: 3, likesCounter: 3 },
+     { user_id: 2, title: 'post3', text: 'beautiful2', commentsCounter: 3, likesCounter: 3 },
+     { user_id: 2, title: 'post4', text: 'beautiful3', commentsCounter: 3, likesCounter: 3 },
+     { user_id: 2, title: 'post5', text: 'beautiful4', commentsCounter: 3, likesCounter: 3 },
+     { user_id: 3, title: 'post6', text: 'beautiful5', commentsCounter: 3, likesCounter: 3 }, ])
+
+Comment.create([{ user_id: 1, post_id:1, text: 'Omg! i love it!' },
+     { user_id: 1,post_id:2, text: 'Omg! i love it!' },
+     { user_id: 2,post_id:1, text: 'Omg! i love it!' },
+     { user_id: 2,post_id:2, text: 'Omg! i love it!' },
+     { user_id: 2,post_id:3, text: 'Omg! i love it!' },
+     { user_id: 3,post_id:1, text: 'Omg! i love it!' }, ])                        
+
+Like.create([{ user_id: 1, post_id:1,  },
+     { user_id: 1,post_id:2 },
+     { user_id: 2,post_id:1},
+     { user_id: 2,post_id:2 },
+     { user_id: 2,post_id:3 },
+     { user_id: 3,post_id:1 }, ])    
