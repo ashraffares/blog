17494b723546b0d8333a3002b02386e4aa4774f5
ashraffares <ashraffares090@gmail.com>
2021-11-20 06:37:20 +0200

    Add like method and update index and show method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,33 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
+  def like
+    Like.create(user_id: params[:data][:user_id])
+    Like.update_likes_counter(params[:data][:post_id])
+    redirect_to posts_show_path(params[:data][:post_id])
+  end
+  
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes([:comments])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { posts: @posts, user: @user } }
+    end
   end
 
   # GET /posts/1 or /posts/1.json
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { post: @post, user: @user } }
+    end
+  end
 
   # GET /posts/new
   def new
